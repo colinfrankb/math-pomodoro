102e557c6a5d53cb643b2de2ed86ebd1c4662b16
Colin <colinfrankb@gmail.com>
2017-10-19 22:15:20 +0200

    main.py added

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+
+thousandth = random.randrange(1, 10) * 1000
+high_hundredth = random.randrange(2, 10)
+high_hundred = high_hundredth * 100
+low_hundred = random.randrange(1, high_hundredth) * 100
+complete_low_hundred = random.randrange(low_hundred, low_hundred + 100)
+subtractee = thousandth + complete_low_hundred
+subtractor = random.randrange(high_hundred, 1000)
+print(subtractee, '-', subtractor)
+
+
+
+
+
+
+
